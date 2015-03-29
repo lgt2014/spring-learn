d64f0dc2645c6f3597391a72e7681385c9e1e7df
lgt <cangruxue@gmail.com>
2015-03-29 16:20:12 +0800

    Add spring dependencies

--- src/main/java/com/lgt/learn/Hello.java
+++ src/main/java/com/lgt/learn/Hello.java
@@ -1,3 +1,4 @@
+package com.lgt.learn;
 class Hello{
 	public static void main(String []args){
 		System.out.println("Hello, lgt");
---
+++ src/main/java/com/lgt/learn/SpringManu.java
@@ -0,0 +1,11 @@
+package com.lgt.learn;
+
+import org.springframework.core.io.ClassPathResource;
+/**
+ * Created by Administrator on 2015/3/29.
+ */
+public class SpringManu {
+    public static void main(String [] args){
+        ClassPathResource res = new ClassPathResource("beans.xml");
+    }
+}
